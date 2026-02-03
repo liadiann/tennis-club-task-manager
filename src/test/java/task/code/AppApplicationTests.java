b58fd9114466c8875f17b7e6ddf6ff6e99c12809
Julia Kon <kon1976y@gmail.com>
2026-02-03 10:21:48 +0300

    create repository

---
+++ src/test/java/task/code/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package task.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
