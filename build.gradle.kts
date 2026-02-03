9ca926bf4ca196aa476c25073379b6345ef59f73
Julia Kon <kon1976y@gmail.com>
2026-02-03 10:51:24 +0300

    add welcome controller

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,22 @@
 plugins {
-	java
-	id("org.springframework.boot") version "4.0.2"
-	id("io.spring.dependency-management") version "1.1.7"
+    application
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("io.freefair.lombok") version "8.6"
+    kotlin("kapt") version "1.9.25"
 }
 
 group = "task"
 version = "0.0.1-SNAPSHOT"
-description = "Demo project for Spring Boot"
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+application {
+    mainClass = "task.code.AppApplication"
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
@@ -19,9 +24,25 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+    testImplementation ("org.instancio:instancio-junit:5.5.1")
+    implementation ("org.springframework.boot:spring-boot-starter-validation")
+    implementation("net.datafaker:datafaker:2.5.1")
+    implementation ("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor ("org.mapstruct:mapstruct-processor:1.6.3")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    implementation ("org.postgresql:postgresql:42.7.7")
+    runtimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
---
+++ src/main/java/task/code/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package task.code.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome!";
+    }
+}
